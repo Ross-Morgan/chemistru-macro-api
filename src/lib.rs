4bd9cd4b98df26d0b51de8286e2365666d9c0c7a
Ross-Morgan <rmorgan512@protonmail.ch>
2023-10-16 12:23:58 +0100

    add element macro and prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
+#[macro_export]
+macro_rules! element {
+    ($mass:literal) => {
+        $crate::elements::PROTON_NUMBER_MAP.get(&$mass).unwrap()
+    };
+}
+
+
 pub mod elements {
     constants::elements_consts!();
     map::element_map!();
+}
+
+pub mod prelude {
+    pub use super::elements::*;
+    pub use super::element;
 }
